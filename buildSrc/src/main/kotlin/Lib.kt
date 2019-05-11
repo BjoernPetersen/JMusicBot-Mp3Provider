54fbf525d5a976e34deeee55c302c456cbcfe3f8
Björn Petersen <pheasn@gmail.com>
2019-05-11 15:43:59 +0200

    Depend on relase version of core

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -2,7 +2,7 @@ object Lib {
     const val KOTLIN = Plugin.KOTLIN
     const val KOTLIN_LOGGING = "1.6.25"
 
-    const val MUSICBOT = "0.22.0-SNAPSHOT"
+    const val MUSICBOT = "0.22.0"
     const val ID3_TAG = "0.9.1"
 
     const val KTOR = "1.1.4"
